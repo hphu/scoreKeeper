779b7369dea58d2be639496cf4c180eaf2a66ac9
Henry Phu <hphu@bu.edu>
2014-11-09 00:03:13 -0500

    fix game winner cannot be equal to loser validation

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,12 @@
+
 class Game < ActiveRecord::Base
 	belongs_to :party
-	belongs_to :winner, class_name: "Player"
 	belongs_to :loser, class_name: "Player"
-	validates :winner_id, presence: true, uniqueness: { scope: :loser_id, message: " cannot be loser!" }
-	validates :loser_id, presence: true
+	belongs_to :winner, class_name: "Player"
+	validate :winner_not_equal_loser
+
+	def winner_not_equal_loser
+		errors.add(:base, "Winner != Loser") unless self.winner_id != self.loser_id
+		
+	end
 end
