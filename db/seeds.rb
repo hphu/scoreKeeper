e46343ce9e2d1e24440475bee7ea5336d4d6b1d1
Ryan Collins <rcollins@apperian.com>
2014-11-19 19:43:19 -0500

    Added create party button to homepage, added seed data script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+party_names = ["ABANDON", "ABANDONED", "ABANDONING", "ABANDONMENT", "ABANDONMENTS","ABANDONS", "ABDICATED", "ABDICATES",
+               "ABDICATING","ABDICATION","ABDICATIONS","ABERRANT","ABERRATION","ABERRATIONAL","ABERRATIONS","ABETTING",
+               "ABNORMAL","ABNORMALITIES","ABNORMALITY","ABNORMALLY","ABOLISH","ABOLISHED","ABOLISHES","ABOLISHING",
+               "ABROGATE","ABROGATED","ABROGATES","ABROGATING","ABROGATION","ABROGATIONS","ABRUPT"]
+
+(0..party_names.length).each do |n|
+  Party.create(:name => party_names[n])
+end
